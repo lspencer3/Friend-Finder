29878ca8f01090dd2ceab94676d5d5f947ec41fb
Lauren <lauren.spencer007@gmail.com>
2017-11-14 00:42:04 -0500

    final updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,8 @@
 var friends = require("../data/friends");
 var allScores = [];
 var results  = [];
+var curBestFriend = [];
+var namesndphotos = [];
 
 module.exports = function(app) {
 
@@ -9,62 +11,96 @@ module.exports = function(app) {
   		res.send(friends);
 	});
 
-
-      // This line is the magic. It"s very similar to the standard ajax function we used.
-      // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
-      // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
-      // depending on if a tables is available or not.
-
 	app.post("/api/friends", function(req, res) {
+		var data = req.body
+	    console.log(data)
+	    console.log(data.uscores)
+	    friends.push(req.body)
+	 	var uname = req.body.name
+		var uphoto = req.body.photo
 
-    console.log(req.body)
+	    namesndphotos.push({
+	    	name: uname,
+	    	photo: uphoto
+	    })
 
-    var scores = [];
+	   // console.log(namesndphotos)
 
-    scores.push(req.body.a1)
-    scores.push(req.body.a2)
-    scores.push(req.body.a3)
-    scores.push(req.body.a4)
-    scores.push(req.body.a5)
-    scores.push(req.body.a6)
-    scores.push(req.body.a7)
-    scores.push(req.body.a8)
-    scores.push(req.body.a9)
-    scores.push(req.body.a10)
+    	//console.log(allScores[0])
 
-    if (allScores[0] != undefined){
-    	results =[];
-    	console.log("comparing other friends...")
-	    for (var i = 0; i < allScores.length; i++){
-	    	var totnum = 0 
-	    	for (var j = 0; j< allScores[i].length; j ++){
-	    		var diff = Math.abs(parseInt(allScores[i][j]) - parseInt(scores[j]))
-	    		totnum += diff
+	    if(allScores[0] === undefined){
+	    	//console.log("You are the first friend")
+	    	var scores = [];
+
+		    scores.push(req.body.uscores[0])
+		    scores.push(req.body.uscores[1])
+		    scores.push(req.body.uscores[2])
+		    scores.push(req.body.uscores[3])
+		    scores.push(req.body.uscores[4])
+		    scores.push(req.body.uscores[5])
+		    scores.push(req.body.uscores[6])
+		    scores.push(req.body.uscores[7])
+		    scores.push(req.body.uscores[8])
+		    scores.push(req.body.uscores[9])
+
+	    	allScores.push(scores)
+
+	    	//console.log(allScores)
+	    	//console.log(namesndphotos)
+	    	res.json(false)
+    	}
+
+    	else if (allScores[0] != undefined){
+    	
+    		var scores = [];
+
+		    scores.push(req.body.uscores[0])
+		    scores.push(req.body.uscores[1])
+		    scores.push(req.body.uscores[2])
+		    scores.push(req.body.uscores[3])
+		    scores.push(req.body.uscores[4])
+		    scores.push(req.body.uscores[5])
+		    scores.push(req.body.uscores[6])
+		    scores.push(req.body.uscores[7])
+		    scores.push(req.body.uscores[8])
+		    scores.push(req.body.uscores[9])
+
+
+	    	console.log(namesndphotos)
+
+	    	curBestFriend = []
+	    	results =[];
+
+	    	console.log("comparing other friends...")
+		    for (var i = 0; i < allScores.length; i++){
+		    	var totnum = 0 
+		    	for (var j = 0; j< allScores[i].length; j ++){
+		    		var diff = Math.abs(parseInt(allScores[i][j]) - parseInt(scores[j]))
+		    		totnum += diff
+	    		}
+	    		results.push(totnum)
 	    	}
-	    	results.push(totnum)
-	    }
 
-	    console.log(results)
+	    	console.log(results)
 
-	    Array.min = function( arr ){
-    		return Math.min.apply(Math, arr );
+	    	Array.min = function( arr ){
+    			return Math.min.apply(Math, arr );
+			};
+			var minimum = Array.min(results);
+			console.log(minimum)
+			//console.log(friends[results.indexOf(minimum)].name)
+			//console.log(friends[results.indexOf(minimum)].photo)
+			//var bfName = friends[results.indexOf(minimum)].name
+			//var bfPhoto = friends[results.indexOf(minimum)].photo
+			var bfName = namesndphotos[results.indexOf(minimum)].name
+			var bfphoto = namesndphotos[results.indexOf(minimum)].photo
+			curBestFriend.push(bfName)
+			curBestFriend.push(bfphoto)
+			console.log(curBestFriend)
+			allScores.push(scores)
+			res.json(curBestFriend)
 		};
-		var minimum = Array.min(results);
-		console.log(minimum)
-		console.log(friends[results.indexOf(minimum)].name)
-		console.log(friends[results.indexOf(minimum)].photo)
-	}
-	else {
-		console.log("You are the first friend")
-	}
 
-    friends.push(req.body);
-
-
-    res.send(friends)
-
-    allScores.push(scores)
-    //console.log(allScores)
 	});
 };
 
